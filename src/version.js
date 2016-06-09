cc5ab7675bbcccf7c9e9f1b688d0748e157cc9bf
Hugheth <thehugheth@gmail.com>
2016-06-09 08:19:01 +0100

    Update supported versions

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports={
-  defaultVersion:'1.8',
-  supportedVersions:['1.7','1.8','15w40b','1.9']
+module.exports = {
+	defaultVersion: '1.10',
+	supportedVersions: [ '1.7', '1.8', '15w40b', '1.9', '1.10' ]
 };
